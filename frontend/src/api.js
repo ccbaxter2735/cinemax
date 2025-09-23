5146abe265ba62a538b2533c91f2c9b0288cba94
ccbaxter <erussard624@gmail.com>
2025-09-24 00:47:57 +0200

    test2

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@ import { ACCESS_TOKEN } from "./constants"
 const apiUrl = "https://656c1b6d-56fe-49d4-b329-706270ee6383.e1-eu-north-azure.choreoapps.dev/choreo-apis/cinemax/backend/v1/";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 })
 
 // Add a request interceptor
